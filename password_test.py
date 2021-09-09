1266a8c22dcbaf0c509c10a8b4fa4ff14116b947
Harry Bett <harrybett89@gmail.com>
2021-09-09 06:52:21 +0300

    create setUp method

--- password_test.py
+++ password_test.py
@@ -7,3 +7,8 @@ class TestUser(unittest.TestCase):
   Args:
       unnittest.TestCase: TestCase class that helps in creating test cases
   '''
+  def setUp(self):
+    '''
+    Set up method to run before each test case
+    '''
+    self.new_user = User("Harry","Bett","0711223344","hb@ms.com")
