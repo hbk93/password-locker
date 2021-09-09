953a314237feef0bbf41c1f1c1b9aba3ee0eb114
Harry Bett <harrybett89@gmail.com>
2021-09-09 06:46:10 +0300

    create TestUser class

--- password_test.py
+++ password_test.py
@@ -2,4 +2,8 @@ import unittest
 from password import User
 
 class TestUser(unittest.TestCase):
-  pass
+  '''
+  Test class that defines test cases for the user class behaviours.
+  Args:
+      unnittest.TestCase: TestCase class that helps in creating test cases
+  '''
