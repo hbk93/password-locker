e46d3069f1a7da79cf075914277cc885ae210270
Harry Bett <harrybett89@gmail.com>
2021-09-09 06:18:17 +0300

    add object properties

--- password.py
+++ password.py
@@ -13,4 +13,7 @@ class User:
         phone_number: User's phone number
         email: User's email address
     '''
-
+    self.first_name = first_name
+    self.last_name = last_name
+    self.phone_number = phone_number
+    self.email = email
